67859b2aafa833498893bcb482aaf92ccb522f68
Kerem <keremdede@protonmail.com>
2025-12-25 12:36:27 +0100

    add analysis of a scene

--- showrunner/scene_analyzer.py
+++ showrunner/scene_analyzer.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, Field
-
+from langchain_ollama import ChatOllama
+from typing import Optional
+from pprint import pprint
 
 # when searching for a scene, what would i need it to reference by?
 # - type? whether it is a comedic, tragic, action, --introductory,--, 
@@ -17,12 +19,37 @@ from pydantic import BaseModel, Field
 # -- what can happen as subtext? following a dream, defending ego, proving someone wrong, etc.
 
 class SceneAnalysis(BaseModel):
-    surface_level_happening: str = Field(description="what is visible on the surface within this scene")
+    happening: str = Field(description="what concrete actions are happening")
     subtext_level_happening: str = Field(description="what is actually told regarding story within this scene")
     reader_reaction: str = Field(description="how the reader should feel when reading this scene")
     
-    
-    
-    
+# model initialization is here because it should not be re-initialized at every function call
+analyzer_model = ChatOllama(
+    model="gpt-oss:20b", 
+    temperature=0,
+    ).with_structured_output(SceneAnalysis)
 
+def analyze_scene(scene: str) -> Optional[SceneAnalysis]:
+
+    system_prompt = {
+        "role": "system",
+        "content": "Analyze the given scene and extract the features from it. Especially take care of getting the subtext correct."
+    }
+
+    analysis_response = analyzer_model.invoke(
+        [
+            system_prompt,
+            {
+                "role": "user",
+                "content": "analyze this scene: {}".format(scene)
+            }
+        ]
+    )
+
+    pprint("analysis_response: \n{}".format(analysis_response))
     
+    try:
+        scene_analysis = SceneAnalysis.model_validate(analysis_response)
+        return scene_analysis
+    except:
+        print("an error occured when validating the model's output")
