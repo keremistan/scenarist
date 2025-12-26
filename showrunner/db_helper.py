ad4af9c7e265078731b9230f6772226601563d5f
Kerem <keremdede@protonmail.com>
2025-12-26 20:19:35 +0100

    add search and delete funcs for chroma

--- showrunner/db_helper.py
+++ showrunner/db_helper.py
@@ -13,6 +13,18 @@ def delete_collection():
     screenplays_vector_store_collection.reset_collection()
     
     
+def delete_docs_by_id(ids: list[str]):
+    screenplays_vector_store_collection.delete(ids)
+    
+def search_docs():
+    res = screenplays_vector_store_collection.get(where_document={"$contains": "0"}, include=['documents'])
+    print(res)
+    
+    
 if __name__ == "__main__":
     # delete_collection() # comment out to delete db mistakenly
+    # search_docs()
+    
+    # ids = []
+    # delete_docs_by_id(ids)
     pass
