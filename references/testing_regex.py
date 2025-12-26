678b54f03a5441420a2db81b447f150d2bfe76a4
Kerem <keremdede@protonmail.com>
2025-12-26 14:38:41 +0100

    clean up previous experiments into 'references' folder

---
+++ references/testing_regex.py
@@ -0,0 +1,15 @@
+import regex as re
+from os import listdir, path
+
+files1 = listdir('/')
+files2 = listdir('./')
+print("files1: {}".format(files1))
+print("files2: {}".format(files2))
+
+with open('./sample outputs/openai gpt-5.2 - second time') as fp:
+    file_content = fp.read()
+    print("file_content: {}".format(file_content))
+    
+    findings = re.findall('--- Style Reference ', file_content)
+    
+    print("findings: {}".format(findings))
